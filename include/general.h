d67b9395ac8af4ce015099dfee458473666d9497
Joey Meijer <meijerjoey@gmail.com>
2021-07-08 18:24:11 +0200

    Added button functionality

---
+++ include/general.h
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+static const uint8_t D0  = (uint8_t)0U;
+static const uint8_t D1  = (uint8_t)1U;
+static const uint8_t D2  = (uint8_t)2U;
+static const uint8_t D3  = (uint8_t)3U;
+static const uint8_t D4  = (uint8_t)4U;
+static const uint8_t D5  = (uint8_t)5U;
+static const uint8_t D6  = (uint8_t)6U;
+static const uint8_t D7  = (uint8_t)7U;
+static const uint8_t D8  = (uint8_t)8U;
+static const uint8_t D9  = (uint8_t)9U;
+static const uint8_t D10 = (uint8_t)10U;
+static const uint8_t D11 = (uint8_t)11U;
+static const uint8_t D12 = (uint8_t)12U;
+static const uint8_t D13 = (uint8_t)13U;
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,133 @@
 #include <Arduino.h>
+#include "general.h"
+
+//---------ArcadeButtons-------------------
+//---------ArcadeButtons_Leds--------------
+
+#define LB_AB_L A0
+#define RB_AB_L A1
+
+#define LW_AB_L A2
+#define RW_AB_L A3
+
+#define LY_AB_L A4
+#define RY_AB_L A5
+
+#define LG_AB_L D3
+#define RG_AB_L D2
+
+//---------ArcadeButtons_Switches----------
+
+#define LB_AB_S D12
+#define RB_AB_S D11
+
+#define LW_AB_S D10
+#define RW_AB_S D9
+
+#define LY_AB_S D8
+#define RY_AB_S D7
+
+#define LG_AB_S D6
+#define RG_AB_S D5
+
+//---------ArcadeButtons END---------------
+//---------variable setup------------------
+
+struct ArcadeSwitch
+{
+  const uint8_t m_LedPin;
+  const uint8_t m_SwitchPin;
+};
+
+ArcadeSwitch Switches[] = 
+{
+  {LB_AB_L, LB_AB_S},
+  {RB_AB_L, RB_AB_S},
+  {LW_AB_L, LW_AB_S},
+  {RW_AB_L, RW_AB_S},
+  {LY_AB_L, LY_AB_S},
+  {RY_AB_L, RY_AB_S},
+  {LG_AB_L, LG_AB_S},
+  {RG_AB_L, RG_AB_S}
+};
+
+uint8_t Button_Values = 0x0;
+uint8_t Prev_Button_Values = 0x0;
+
+//---------variable setup END--------------
+//---------Forward declared functions------
+
+void ButtonSetup();
+void CheckForMultipleBits();
+
+//---------Forward declared functions END--
 
 void setup() {
-  // put your setup code here, to run once:
+  ButtonSetup();
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Button_Values = 0x0;
+
+  for(int i = 0; i < 8; i++)
+    Button_Values |= (digitalRead(Switches[i].m_SwitchPin) << i);
+
+  CheckForMultipleBits();
+
+  if(Button_Values != Prev_Button_Values && Button_Values != 0xFF)
+  {
+    Prev_Button_Values = Button_Values;
+    for(int i = 0; i < 8; i++)
+    {
+      digitalWrite(Switches[i].m_LedPin, (Button_Values & (1 << i)) ? LOW : HIGH);
+    }
+  }
+  //Serial.println(Button_Values, BIN);
+  delay(50);
+}
+
+void ButtonSetup()
+{
+  // --Leds------
+  pinMode(Switches[0].m_LedPin, OUTPUT);
+  pinMode(Switches[1].m_LedPin, OUTPUT);
+  pinMode(Switches[2].m_LedPin, OUTPUT);
+  pinMode(Switches[3].m_LedPin, OUTPUT);
+  pinMode(Switches[4].m_LedPin, OUTPUT);
+  pinMode(Switches[5].m_LedPin, OUTPUT);
+  pinMode(Switches[6].m_LedPin, OUTPUT);
+  pinMode(Switches[7].m_LedPin, OUTPUT);
+
+  digitalWrite(Switches[0].m_LedPin, LOW);
+  digitalWrite(Switches[1].m_LedPin, LOW);
+  digitalWrite(Switches[2].m_LedPin, LOW);
+  digitalWrite(Switches[3].m_LedPin, LOW);
+  digitalWrite(Switches[4].m_LedPin, LOW);
+  digitalWrite(Switches[5].m_LedPin, LOW);
+  digitalWrite(Switches[6].m_LedPin, LOW);
+  digitalWrite(Switches[7].m_LedPin, LOW);
+
+  // --Switches--
+  pinMode(Switches[0].m_SwitchPin, INPUT_PULLUP);
+  pinMode(Switches[1].m_SwitchPin, INPUT_PULLUP);
+  pinMode(Switches[2].m_SwitchPin, INPUT_PULLUP);
+  pinMode(Switches[3].m_SwitchPin, INPUT_PULLUP);
+  pinMode(Switches[4].m_SwitchPin, INPUT_PULLUP);
+  pinMode(Switches[5].m_SwitchPin, INPUT_PULLUP);
+  pinMode(Switches[6].m_SwitchPin, INPUT_PULLUP);
+  pinMode(Switches[7].m_SwitchPin, INPUT_PULLUP);
+}
+
+void CheckForMultipleBits()
+{
+  bool HasBeenSet = false;
+  for(int i = 0; i < 8; i++)
+  {
+    if(!(Button_Values & (1 << i)) && HasBeenSet == false)
+      HasBeenSet = true;
+    else if(!(Button_Values & (1 << i)) && HasBeenSet == true)
+      Button_Values |= (1 << i);
+  }
 }
